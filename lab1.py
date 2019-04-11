b2a645c341136c45c6235ae9ad11a276561fe68b
Nathan Holland <nwhollan@calpoly.edu>
2019-04-10 19:55:31 -0700

    Altered code to increase coverage

--- lab1.py
+++ lab1.py
@@ -36,9 +36,10 @@ def bin_search(target, low, high, int_list):  # must use recursion
    if (high - low) == 1:
       return None
    if len(int_list) == 1:
-       if int_list[0] == target:
-          return 0
-       elif int_list[0] != target:
+       #if int_list[0] == target:
+          #return 0
+       #el
+       if int_list[0] != target:
           return None
    if int_list[(high + low)//2] > target:
       high = (high + low)//2
